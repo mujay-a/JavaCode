2d9e248117e3e9f79b9b064253ae65e0514c3f7c
MUJAY A <mujaymujay7@gmail.com>
2024-03-26 17:58:13 +0530

    added a backend

--- Hello.java
+++ Hello.java
@@ -3,7 +3,12 @@ class Hello{
         int a = 12;
         int b = 8;
         //for eg think this is an frontend
-        
+        if(a >=b){
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
         System.out.println(a + b);
         System.out.println(a * b);
     }
