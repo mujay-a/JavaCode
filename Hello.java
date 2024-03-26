679d655b6c7c70706ff2f0dd993c9d6032455ce6
MUJAY A <mujaymujay7@gmail.com>
2024-03-26 17:52:52 +0530

    added some frontend code

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ class Hello{
     public static void main(String[] args) {
         int a = 12;
         int b = 8;
+        //for eg think this is an frontend
+        
         System.out.println(a + b);
         System.out.println(a * b);
     }
